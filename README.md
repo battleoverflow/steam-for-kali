98e65f5f775707d3e438f82aa2709ffbed602447
Hifumi <hifumisecurity@protonmail.com>
2022-04-27 15:07:07 -0500

    Better

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# steam-for-kali
+# Steam for Kali
 Bash script created to install the Steam Launcher on Kali Linux
+
+*Please be aware, installing Steam and it's dependencies require ~400mb+*
+
+## Usage
+```bash
+# Make the bash script an executable
+chmod +x steam.sh
+
+# Run the script
+./steam.sh
+```
--- steam.sh
+++ steam.sh
@@ -1,12 +1,51 @@
 #!/bin/bash
 
-wget http://media.steampowered.com/client/installer/steam.deb
+##########################################################
+# Creator: Hifumi1337                                    #
+# Version: 0.1.3                                         #
+# Project: https://github.com/Hifumi1337/steam-for-kali  #
+##########################################################
 
-sudo apt install ./steam.deb
+echo ""
+echo "Creator: Hifumi1337 (https://github.com/Hifumi1337)"
+echo "Version: 0.1.3"
+echo "Project: https://github.com/Hifumi1337/steam-for-kali"
+echo ""
 
-sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt install libgl1-mesa-dri:i386 libgl1:i386
-sudo apt install nvidia-driver-libs:i386
+function installSteam() {
+    wget http://media.steampowered.com/client/installer/steam.deb
 
-steam
+    # Add the i386 architecture
+    sudo dpkg --add-architecture i386
+
+    # Pull down new architecture
+    sudo apt-get update
+    
+    # Shared Objects & Drivers (Dependencies)
+    sudo apt install libgl1-mesa-dri:i386 libgl1:i386
+    sudo apt install nvidia-driver-libs:i386
+
+    # Install Steam
+    sudo apt install ./steam.deb
+
+    # Remove debian clutter
+    sudo rm steam.deb
+}
+
+installSteam
+
+function testSteam(){
+    steam
+}
+
+
+echo -n "Would you like to open Steam (y/n)? "
+read openSteam
+
+if [ "$openSteam" == "y" ]
+then
+    testSteam
+else
+    echo ""
+    echo "Thank you for using Steam for Kali. Have a good day!"
+fi
